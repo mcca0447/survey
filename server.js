e2aec929019ad8556b60f21a1f640869221432b6
mcca0447 <mccabe.james24@gmail.com>
2018-07-02 15:54:12 -0400

    remove old server file

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-app.use(express.static('public'))
-
-app.use(express.static('public'))
-
-app.listen(process.env. Port || 8080, () => console.log('ALL is well!'))
