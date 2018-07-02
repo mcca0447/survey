c7702a01ac5edd8e437ff8fabbd9c735ec576014
mcca0447 <mccabe.james24@gmail.com>
2018-07-02 15:48:54 -0400

    added server

---
+++ app.js
@@ -0,0 +1,47 @@
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+//Connect to postgres
+
+
+
+// END
+
+
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.set('view engine', 'ejs');
+
+app.use('/css', express.static('css'));
+
+
+app.get('/index', function(req, res){
+    res.sendFile(__dirname +'/index.html');
+});
+
+app.post('/survey', urlencodedParser, function(req, res){
+   console.log(req.body);
+   res.sendFile(__dirname +'/index.html', {qs: req.query});
+});
+
+
+
+//IF there is a POST request
+    //Pull data from the form
+    //write field values to appropriate backend objs
+    //Connect to postgress database
+    //write entry to database using object values
+    //close connection to db
+    //fetch the matching entry from the database
+    //write it to a confirmation screen
+
+
+
+app.listen(3000);
+
+console.log('Dialed in and now listening on port 3000');
+
+
